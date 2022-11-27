ee5e07ad40030bf828bc0fa129adc035988602e8
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2022-11-27 18:31:24 +0200

    66yyyt

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-
+const refs = {
+    bodyColor: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+  };
+  refs.stopBtn.setAttribute('disabled', true);
+  const getRandomHexColor = () => {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+  let timerId = null;
+  
+  refs.startBtn.addEventListener('click', () => {
+    timerId = setInterval(() => {
+      refs.startBtn.setAttribute('disabled', true);
+      refs.stopBtn.removeAttribute('disabled');
+      refs.bodyColor.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  });
+  
+  refs.stopBtn.addEventListener('click', () => {
+    refs.stopBtn.setAttribute('disabled', true);
+    refs.startBtn.removeAttribute('disabled');
+    clearInterval(timerId);
+  });
